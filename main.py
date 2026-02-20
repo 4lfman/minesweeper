e42164848dbfd784056dc0017f8a0e2958abdb32
Alfred Sjögren <31034983+4lfman@users.noreply.github.com>
2026-02-20 11:06:53 +0100

    add clickable board and reset

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QGridLayout, QMainWindow
 from PyQt5.QtCore import pyqtSlot
 from PyQt5 import QtCore
+from PyQt5.QtGui import QFont
 
 def start_game():
     print("Let the games begin!")
@@ -33,31 +34,80 @@ class Window(QMainWindow):
 
         self.main_widget.setLayout(layout)
 
-        #window.show()
-        #width = 400
-        #window = QWidget()
-        #window.setWindowTitle("Minesweeper!")
-        #window.setFixedWidth(width)
-        #window.setFixedHeight(width)
+class Minesweeper(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.main_widget = QWidget()
+        self.setCentralWidget(self.main_widget)
 
-        #label = QLabel("Welcome to Minesweeper!")
-        #label.setAlignment(QtCore.Qt.AlignCenter)
+        self.setWindowTitle("The game has begun!")
 
-        #button = QPushButton("Start game")
-        #button.clicked.connect(start_game)
-        #button.setFixedHeight(50)
+        self.UiComponents()
+        
+        width = 400
+        height = width
+        self.setFixedSize(width,height)
 
-        #layout = QVBoxLayout()
-        #layout.addWidget(label)
-        #layout.addWidget(button)
+        self.show()
 
-        #window.setLayout(layout)
+    def UiComponents(self):
+        self.turn = 0
+        
+        # Square map side length
+        self.gridSize = 6
+        
+        # Add pressable buttons
+        mapGrid = QGridLayout()
+        self.buttonList = []
+        for i in range(self.gridSize):
+            innerList = []
+            for j in range(self.gridSize):
+                tmpBtn = QPushButton()
+                innerList.append(tmpBtn)
+                mapGrid.addWidget(tmpBtn, i, j)
 
-        #window.show()
+            self.buttonList.append(innerList)
+       
+        self.main_widget.setLayout(mapGrid) 
+
+        # Add functionality to the buttons
+        x = 90
+        y = 90
+        for i in range(self.gridSize):
+            for j in range(self.gridSize):
+                #self.buttonList[i][j].setGeometry(x*i + 20,
+                #                                  y*j + 20,
+                #                                  80, 80)
+                self.buttonList[i][j].clicked.connect(self.actionCalled)
+                self.buttonList[i][j].setFont(QFont(QFont("Times", 25)))
+
+        # Button to reset the game to the starting position
+        resetButton = QPushButton()
+        resetButton.clicked.connect(self.resetGame)
+        resetButton.setText("Reset Game")
+        mapGrid.addWidget(resetButton, self.gridSize, 0, 1, self.gridSize)
+
+    # Called when a field is pressed
+    def actionCalled(self):
+        self.turn += 1
+
+        button = self.sender()
+
+        button.setEnabled(False)
+        button.setText("X")
+
+    def resetGame(self):
+        self.turn = 0
+
+        for i in range(self.gridSize):
+            for j in range(self.gridSize):
+                self.buttonList[i][j].setText("")
+                self.buttonList[i][j].setEnabled(True)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = Window()
+    window = Minesweeper()
+    #window = Window()
     window.show()
     sys.exit(app.exec_())
